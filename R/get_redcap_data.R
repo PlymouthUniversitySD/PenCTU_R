aaaffb7dcde93281a9b6f16cb9db9bbbb612b99b
paaigan <paigan.aspinall@plymouth.ac.uk>
2024-02-13 14:02:49 +0000

    Updated the function to manage use of ontology services, annotated code.

--- R/get_redcap_data.R
+++ R/get_redcap_data.R
@@ -1,5 +1,5 @@
 # Author: Matthew Bailey
-# Date: 23JAN2024
+# Date & version: 23JAN2024 V1.1.0
 # R version: 4.3.1
 #' Get data from REDCap using an API token
 #'
@@ -55,7 +55,7 @@ get_redcap_data <- function(api_token, test = FALSE){
   
   ##LABEL VARIABLES
   ##Get data dictionary
- 
+  
   metadata_request <- list(token=api_token,
                            content='metadata',
                            format='csv',
@@ -65,42 +65,57 @@ get_redcap_data <- function(api_token, test = FALSE){
   
   data_dictionary <- httr::content(metadata_response, show_col_types = FALSE)
   
+  # Extract field names and labels from data_dictionary and pivot them into wide format
   variable_labels <- data_dictionary %>%
     dplyr::select(field_name, field_label) %>%
     tidyr::pivot_wider(names_from = field_name, values_from = field_label)
   
- 
-  ## Find matching labels and remove any html tags and colons
-  
+  # Function to remove HTML tags, asterisks, vertical bars, and colons from labels
   remove_html <- function(htmlString) {
     return(sub("[*|:]*$", "", gsub("<.*?>", "", htmlString)))
   }
   
+  # Apply remove_html function to labels in variable_labels and store the result in label_names
   label_names <- lapply(as.list(variable_labels[1, intersect(names(record_data), names(variable_labels)), with=FALSE]), remove_html)
   
+  # Assign labels to variables in record_data using expss::var_lab function
   for (each_name in names(record_data)) {
     expss::var_lab(record_data[[each_name]]) <- label_names[[each_name]]
   }
   
-  ##LABEL VALUES
-  data_dictionary <<- dplyr::mutate(data_dictionary, across(everything(), ~ ifelse(. == '', NA, .))) # Ensure blank dictionary items are marked as NA
+  # Ensure that any empty strings in data_dictionary are replaced with NA
+  data_dictionary <<- dplyr::mutate(data_dictionary, across(everything(), ~ ifelse(. == '', NA, .)))
   
+  # Loop through each row in data_dictionary and process select_choices_or_calculations
   for (i in 1:nrow(data_dictionary)) {
     var_name <- data_dictionary[["field_name"]][i]
+    
     if (!is.null(record_data[[var_name]])) {
-      if ( !is.na(data_dictionary[["select_choices_or_calculations"]][i]) && data_dictionary[["field_type"]][i] != "calc" && data_dictionary[["field_type"]][i] != "slider") {
+      if (!is.na(data_dictionary[["select_choices_or_calculations"]][i]) && 
+          data_dictionary[["field_type"]][i] != "calc" && 
+          data_dictionary[["field_type"]][i] != "slider") {
+        
+        # Extract choices and labels, then convert record_data to factors with specified levels and labels
         value <- stringr::str_remove_all(data_dictionary[["select_choices_or_calculations"]][i], '\"') %>% 
-          stringr::str_split_1(' \\| ') %>% 
-          stringr::str_trim() %>% 
-          stringr::str_split(", ", n = 2)
+          stringr::str_trim()
         
-        levels <- sapply(value, `[[`, 1)
-        labels <- sapply(value, `[[`, 2)
-        
-        record_data[[var_name]] <- factor(record_data[[var_name]], levels = levels, labels = labels)
+        if (grepl("\\|", value)) {
+          value <- stringr::str_split_1(value, ' \\| ') %>% 
+            stringr::str_trim() %>% 
+            stringr::str_split(", ", n = 2)
+          
+          levels <- sapply(value, `[[`, 1)
+          labels <- sapply(value, `[[`, 2)
+          
+          record_data[[var_name]] <- factor(record_data[[var_name]], levels = levels, labels = labels)
+        } else {
+          # Handle the case where there's no delimiter
+          record_data[[var_name]] <- factor(record_data[[var_name]], levels = c(value), labels = c(value))
+        }
       }
     }
   }
   
+  # Return modified record_data
   return(record_data)
 }
