65cf545c79f2af8d65f371c0b61bce0045cb0bd4
ReeceTarrant <rtarrant70998@gmail.com>
2025-05-16 12:15:15 +0100

    Updated function by including error handling to check that an actual row is being provided to the function.

--- R/calculate_completeness.R
+++ R/calculate_completeness.R
@@ -1,9 +1,9 @@
 # Author: Paigan Aspinall
 # Date: 23JAN2024
 # R version: 4.2.2
-#' Calculates the completeness of a timepoint based on the *timepoint_name*_compelte column values.
+#' Calculates the completeness of a timepoint based on the *timepoint_name*_complete column values.
 #'
-#' Outputs "Complete", "Partially complete", "Not started or "Undefined" as the completeness value for a row in a defined dataset.Used in conjundtion with plot_crf_completeness_data_preparation function.
+#' Outputs "Complete", "Partially complete", "Not started or "Undefined" as the completeness value for a row in a defined dataset.Used in conjunction with plot_crf_completeness_data_preparation function.
 #'
 #' @param row A row defined from a dataset.
 #'
@@ -20,11 +20,24 @@
 #'
 
 calculate_completeness <- function(row) {
-  if (any(!is.na(row)) && ((any(row %in% c(1, 2)) && any(row == 0)) || (any(row == 1) && any(row == 2)))) {
+  if(is.null(row)) {
+    stop("Row not provided!")
+  }
+  
+  if (
+    any(!is.na(row)) && (
+      (any(row %in% c(1, 2), na.rm = TRUE) && any(row == 0, na.rm = TRUE)) ||
+      (any(row == 1, na.rm = TRUE) && any(row == 2, na.rm = TRUE))
+    )
+  ) {
     return("Partially complete")
-  } else if (all(row == 2, na.rm = TRUE) || all(is.na(row) | row == 2)) {
+  } else if (
+    all(row == 2, na.rm = TRUE) || all(is.na(row) | row == 2)
+  ) {
     return("Complete")
-  } else if (all(is.na(row)) || all(row == 0) || all(is.na(row) | row == 0)) {
+  } else if (
+    all(is.na(row)) || all(row == 0, na.rm = TRUE) || all(is.na(row) | row == 0)
+  ) {
     return("Not started")
   } else {
     return("Undefined")
