b1f8da520c89cc241a79f58cc90b08a09c022ca8
paaigan <paigan.aspinall@plymouth.ac.uk>
2026-07-07 11:38:30 +0100

    Added templates for cross contact upload

--- Templates/Cross-project Contacts/Template_ContactUploadScript.R
+++ Templates/Cross-project Contacts/Template_ContactUploadScript.R
@@ -1,4 +1,4 @@
-#Title: AUtomated Script for Study Contact Upload
+#Title: Automated Script for Study Contact Upload - TEMPLATE 
 #Author: Paigan Aspinall
 #Version & Date: V1.0.0 02JUL2026
 #R version: 4.4.3
@@ -8,8 +8,11 @@ library(dplyr)
 library(httr)
 library(jsonlite)
 
-#iImport the mapping csv defining the fields to be uploaded
-mapping <- read.csv("BOOST_ContactUpload.csv")
+#Import the mapping csv defining the fields to be uploaded
+mapping <- read.csv("XXX_ContactUpload.csv")
+
+#Define your study name
+study_name <- "STUDY NAME"
 
 #Define the REDCap URL
 url <- "https://clinicaltrials-pre.plymouth.ac.uk/api/"
@@ -17,3 +20,8 @@ url <- "https://clinicaltrials-pre.plymouth.ac.uk/api/"
 
 #Run the mapping function
 results <- cross_project_contact_upload(mapping, url)
+
+#Save the report out
+today <- Sys.Date()
+file_name <- paste0(study_name, "_ContactUploadReport_", today, ".csv")
+write.csv(results, file_name)
